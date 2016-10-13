74f0ab13b6cef648b651b672b34104beafa926c9
Sarah Veness <sarahhveness@gmail.com>
2016-10-13 22:24:21 +0000

    route set up for users pages

--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,16 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resource :users, only: [:new] do
+    get '/signup', to: 'users#new'
+  end
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
 
-  # root to: 'categories#index'
-
-  # resources :categories, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
