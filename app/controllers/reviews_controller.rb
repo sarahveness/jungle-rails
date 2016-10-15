9b918108f8f5a33298e5ac08b4983ff979f038e2
Sarah Veness <sarahhveness@gmail.com>
2016-10-15 00:57:06 +0000

    users can leave reviews on specific products

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,33 @@
 class ReviewsController < ApplicationController
 
+  def new
+    @review = Review.new
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
   def create
-  raise "Yay, I'm here!"
-end
+    puts review_params
+    @review = Review.new(review_params)
+    @product = Product.find(params["product_id"])
+    @review.product_id = params["product_id"]
+    @review.rating = params["rating"].to_i
+    @review.user = @current_user
+    if @review.save
+      flash[:success] = "Thanks for leaving a review!"
+      redirect_to :back
+    else
+      puts @review.errors.to_a
+    end
+  end
+
+
+private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
 
 end
